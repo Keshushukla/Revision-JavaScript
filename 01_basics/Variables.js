42b00a2a45c19d14b786c293026a0686c62c8f1a
Keshu <2003013053@ipec.org.in>
2023-10-31 12:05:43 +0530

    Variables Updation and printing

--- 01_basics/Variables.js
+++ 01_basics/Variables.js
@@ -2,11 +2,20 @@
 const accountId = 123; 
 let accountEmail = "shukla@abc.com";
 var accountPassword ="9889"
+// Variable declaration without var,let aur const
  accountCity = "Noida";
+ accountCity = "Ghaziabad";
+ console.log(accountCity);
 
 
 // Updating the values of let, Const and Var
+// accountId = 12; is not allowed
+accountEmail = "shukla@abcd.com";
+accountPassword ="989"
+
 // Printing the Values 
 // 1) One-by-one
+console.log(accountId);
 // 2) Together as a Table 
+console.table([accountId, accountEmail, accountPassword, accountCity]);
 // Variable output without assigning any value to it or Empty Variable output 
