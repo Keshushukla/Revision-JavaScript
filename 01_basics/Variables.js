a66524e9907140c5f9249bce126da6c56eb6cfec
Keshu <2003013053@ipec.org.in>
2023-10-31 11:59:53 +0530

    Variables Declaration

---
+++ 01_basics/Variables.js
@@ -0,0 +1,12 @@
+// Declaration of let, Const and Var
+const accountId = 123; 
+let accountEmail = "shukla@abc.com";
+var accountPassword ="9889"
+ accountCity = "Noida";
+
+
+// Updating the values of let, Const and Var
+// Printing the Values 
+// 1) One-by-one
+// 2) Together as a Table 
+// Variable output without assigning any value to it or Empty Variable output 
